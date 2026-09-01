710f1cbd0ec69ce0766d288aae4e906289e6f40e
VM-Pranesh <vm_pranesh@gmail.com>
2026-09-01 21:03:39 +0530

    ETLEAPCP-1268/SE4-290: Maven Project and the Dependency Ban
    Add Maven Project Files

---
+++ sprint-05/src/test/java/org/leap/domain/DomainTest.java
@@ -0,0 +1,13 @@
+package org.leap.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DomainTest {
+
+    @Test
+    void testSetup() {
+        assertTrue(true);
+    }
+}
